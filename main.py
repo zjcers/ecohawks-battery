e12596fc424b26ac9cc0be8095a8845a5bc2c637
Zane J Cersovsky <zcersovsky@gmail.com>
2016-03-29 21:28:43 -0500

    Updated initialization/bootstrap code

--- main.py
+++ main.py
@@ -2,63 +2,38 @@
 #Original Author: Zane J Cersovsky
 #Original Date: Mar 8 2016
 #Last Modified By: Zane J Cersovsky
-#Last Modified On: Mar 8 2016
+#Last Modified On: Mar 23 2016
 
-#import python standard modules
-import sys
-import threading
+#Load required STL modules
 import os
-#set variables
-try:
-	TESTING = bool(os.environ["PB_TESTING"])
-except:
-	TESTING = False
-try:
-	LOCAL_LUX = bool(os.environ["PB_LOCAL_LUX"])
-except:
-	LOCAL_LUX = False
-try:
-	CONFIG_FILE = bool(os.environ["PB_CONFIG_FILE"])
-except:
-	CONFIG_FILE = "default.conf"
-#import custom modules
-if TESTING:
-	import dummyluxsensor as luxsensor
-	import dummyvoltagesensor as voltagesensor
-	import dummycurrentsensor as currentsensor
-	import dummyrelay as relay
-else:
-	if LOCAL_LUX:
-		import localluxsensor as luxsensor
-	else:
-		import luxsensor
-	import voltagesensor
-	import currentsensor
-	import relay
-import sensorthread
+import logging
+#set up logging
+logging.getLogger("PB").setLevel(logging.INFO)
+logging.basicConfig(format="[%(asctime)s %(name)s] %(message)s")
+mainLogger = logging.getLogger("PB.main")
+mainLogger.info("Prius Battery control program initializing")
+#Load config modules
+mainLogger.info("Setting up configuration")
 import config
-class BatteryManager():
-	def __init__(self, config):
-		self.shutdownEvent = threading.Event()
-		self.currentInS = currentsensor.CurrentSensor(bus=config["bus"], addr=config["currentInAddr"])
-		self.currentInThread = sensorthread.SensorThread(self.currentInS, self.shutdownEvent)
-		self.currentOutS = currentsensor.CurrentSensor(bus=config["bus"], addr=config["currentOutAddr"])
-		self.currentOutThread = sensorthread.SensorThread(self.currentOutS, self.shutdownEvent)
-		self.voltageS = voltagesensor.VoltageSensor(bus=config["bus"], addr=config["voltageAddr"])
-		self.voltageThread = sensorthread.SensorThread(self.voltageS, self.shutdownEvent)
-		self.luxS = luxsensor.LuxSensor(bus=config["bus"], addr=config["luxAddr"])
-		self.luxThread = sensorthread.SensorThread(self.luxS, self.shutdownEvent)
-		self.relay = relay.USBRelay(port=config["port"])
-	def startThreads(self):
-		self.currentInThread.start()
-		self.currentOutThread.start()
-		self.luxThread.start()
-	def returnReadings(self):
-		return self.currentInThread.reading, self.currentOutThread.reading, self.voltageThread.reading
+import cfg
+if "PB_CONFIG_FILE" in os.environ:
+	cfg.cfg = config.config(os.environ["PB_CONFIG_FILE"])
+else:
+	cfg.cfg = config.config("conf/dummy.conf")
+if "debug" in cfg.cfg:
+	logging.getLogger("PB").setLevel(logging.DEBUG)
+	mainLogger.debug("Debugging enabled")
+#import the following pluggable modules (as defined in conf file):
+#currentInSensor: dummycurrentsensor | currentsensor
+#currentOutSensor: dummycurrentsensor | currentsensor
+#luxSensor: dummyluxsensor | localluxsensor | remoteluxsensor
+#voltageSensor: dummyvoltagesensor | voltagesensor
+#batteryManager: basicbatterymanager
+#batteryController: simplebatterycontroller
+#actor: dummyactor
+import imports
+#this is now aliased by imports
+import actor
 if __name__ == "__main__":
-	try:
-		cfg = config.config(CONFIG_FILE)
-	except:
-		cfg = config.config()
-	bm = BatteryManager(cfg)
-	bm.startThreads()
+	mainActor = actor.Actor()
+	mainActor.run()
--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-export PB_CONFIG_FILE = "test.conf"
-export PB_TESTING = "True"
-export PB_LOCAL_LUX = "True"
-export PYTHON_PATH = $PYTHON_PATH:"dummydrivers":"utility":"drivers"
-python main.py
+#Original Author: Zane J Cersovsky
+#Original Date: Mar 10 2016
+#Last Modified By: Zane J Cersovsky
+#Last Modified On: Mar 23 2016
+set -e
+#finds the base directory of the project from the location of this script
+export PB_BASE_DIR=$(dirname $0)
+#checks if a config file was passed, if not, uses the default one
+if [ -z "$1" ]
+  then
+    export PB_CONFIG_FILE=$PB_BASE_DIR/conf/default.conf
+  else
+    export PB_CONFIG_FILE=$PB_BASE_DIR/conf/$1
+fi
+#shows python where all of the custom modules are
+export PYTHONPATH=$PYTHONPATH":dummydrivers:utility:drivers:batterymanagers:actors:sensorLoggers"
+#starts the project
+python -i main.py
